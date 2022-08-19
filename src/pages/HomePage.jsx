df80bf2463592e58d581a18cce1a479f3f1031c4
Alex <alexanderstubmichelsen@gmail.com>
2022-08-19 18:30:32 +0200

    first comit 2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
+import backgroundImage from "../img/bg/WIN_20220819_17_00_22_Pro.jpg"; 
+ 
 export default function Home() {
-    return <>
+
+    
+
+    return (
+        <>
+        <div style={{ 
+        backgroundImage: 'url(' + backgroundImage + ')', 
+        width: "112vmin",
+        height: "80vmin"}}>
             <h1>Home</h1>
             <p>Content from home goes here.</p>
-        </>
+            </div>
+            </>
+    );
+        
 }
